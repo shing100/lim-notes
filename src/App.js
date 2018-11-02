88b371214e152e1844c2bcd0f14bdb80f6a7d4b3
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2018-11-02 11:00:53 +0900

    Edit Note Mutation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import { Query } from "react-apollo";
 import { GET_NOTES } from "./queries";
 
-/*<Query query={GET_NOTES}>{() => null}</Query>*/
-
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Query query={GET_NOTES}>{() => null}</Query>
       </div>
     );
   }
