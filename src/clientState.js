4fe5825aff2fb8751fbb13a0b0b6c018080f0084
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2018-11-03 13:22:36 +0900

    localStroge save

--- src/clientState.js
+++ src/clientState.js
@@ -1,13 +1,9 @@
 import { NOTE_FRAGMENT } from "./fragments";
 import { GET_NOTES } from "./queries";
+import { saveNotes } from "./offline";
 
 export const defaults = {
-    notes: [{
-        __typename: "Note",
-        id: 1,
-        title: "First",
-        content: "Second"
-    }]
+    notes: []
 };
 export const typeDefs = [
     `
@@ -59,6 +55,7 @@ export const resolvers = {
                     notes: [newNote, ...notes]
                 }
             })
+            saveNotes(cache);
             return newNote;
         },
         editNote: (_, {id, title, content}, { cache } ) => {
@@ -77,6 +74,7 @@ export const resolvers = {
                 fragment: NOTE_FRAGMENT,
                 data: updatedNote
             })
+            
             return updatedNote;
         }
     }
